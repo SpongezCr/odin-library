dac75638fe8114b8af498a29e0cafffff6dc15a2
SpongezCr <gavinn0000@gmail.com>
2025-07-04 17:58:23 +0800

    Finish all basic functionalities
    There is currently no CSS styling.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, numOfPages, haveRead) {
     if (!new.target) throw Error("Need to use 'new' operator!");
@@ -9,25 +9,85 @@ function Book(title, author, numOfPages, haveRead) {
     this.author = author;
     this.numOfPages = numOfPages;
     this.haveRead = haveRead;
-    this.info = () => `${this.title} by ${this.author}, ${numOfPages} pages, ${haveRead ? "already read" : "not read yet"}`
 }
 
+Book.prototype.toggleRead = function () {
+    this.haveRead = !this.haveRead;
+}
+
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.haveRead ? "already read" : "not read yet"}`;
+}
 
 function addBookToLibrary(title, author, numOfPages, haveRead) {
     myLibrary.push(new Book(title, author, numOfPages, haveRead));
 }
 
+function removeBook() {
+    let id = this.getAttribute('data-id');
+    myLibrary = myLibrary.filter(book => book.id !== id);
+    displayBooks();
+}
+
+function toggleReadBook() {
+    let id = this.getAttribute('data-id');
+    let theBook = myLibrary.find(book => book.id === id);
+    theBook.toggleRead();
+    displayBooks();
+}
+
+function displayBook(book) {
+    const flexbox = document.createElement("div");
+    flexbox.classList.add("flexbox");
+
+    const bookInfo = document.createElement("p");
+    bookInfo.textContent = book.info();
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete this book";
+    deleteButton.setAttribute("data-id", book.id);
+    deleteButton.addEventListener('click', removeBook);
+
+    const readButton = document.createElement("button");
+    readButton.textContent = book.haveRead ? "Unread" : "Read";
+    readButton.setAttribute("data-id", book.id);
+    readButton.addEventListener('click', toggleReadBook);
+
+    flexbox.appendChild(bookInfo);
+    flexbox.appendChild(deleteButton);
+    flexbox.appendChild(readButton);
+    container.appendChild(flexbox);
+
+}
+
 function displayBooks() {
-    myLibrary.forEach( book => {
-        const bookInfo = document.createElement("p");
-        bookInfo.textContent = book.info();
-        container.appendChild(bookInfo);
-    })
+    container.innerHTML = "";
+    myLibrary.forEach(displayBook);
 }
 
 const container = document.querySelector(".container");
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hobbit1", "J.R.R. Tolkien1", 291, false);
+addBookToLibrary("The Hobbit2", "J.R.R. Tolkien2", 292, false);
+addBookToLibrary("The Hobbit3", "J.R.R. Tolkien3", 293, false);
 displayBooks();
+
+const dialog = document.querySelector("dialog");
+
+const openButton = document.querySelector("#open-button");
+const closeButton = document.querySelector('#close-button');
+const submitButton = document.querySelector('#submit-button')
+
+const inputs = dialog.querySelectorAll('input');
+
+openButton.addEventListener('click', () => dialog.showModal());
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const inputArray = Array.from(inputs).map(item => item.value);
+    addBookToLibrary(inputArray[0], inputArray[1], inputArray[2], false);
+    displayBooks();
+    dialog.close();
+    document.querySelector('form').reset();
+})
+
